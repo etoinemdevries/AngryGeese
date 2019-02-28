4691c5505f09fb6f4157e4a3b3a2a6493edc4e2a
Ed <215186@edu.rocfriesepoort.nl>
2019-02-28 13:59:05 +0100

    Did some import shit?

---
+++ app/src/main/java/com/teamsenseo/angrygeese/activities/FinalReportActivity.java
@@ -0,0 +1,15 @@
+package com.teamsenseo.angrygeese.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import com.teamsenseo.angrygeese.R;
+
+public final class FinalReportActivity extends FragmentActivity {
+
+    @Override
+    protected final void onCreate(final @Nullable Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_final_report);
+    }
+}
