a39b3ab0c9a38e546bdd8bd52fc8ee00add6ec7f
Robert <robertkremer.frl@gmail.com>
2019-03-07 11:49:42 +0100

    woooosh

--- app/src/main/java/com/teamsenseo/angrygeese/activities/MapActivity.java
+++ app/src/main/java/com/teamsenseo/angrygeese/activities/MapActivity.java
@@ -1,8 +1,6 @@
 package com.teamsenseo.angrygeese.activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
@@ -10,12 +8,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
 import com.teamsenseo.angrygeese.R;
 import com.teamsenseo.angrygeese.utils.FileUtils;
-
-import org.xmlpull.v1.XmlPullParser;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -26,7 +20,6 @@ import java.util.ArrayList;
  * @author Robert
  */
 public final class MapActivity extends FragmentActivity implements OnMapReadyCallback {
-    private GoogleMap map;
 
     @Override
     protected final void onCreate(final @Nullable Bundle bundle) {
@@ -43,12 +36,6 @@ public final class MapActivity extends FragmentActivity implements OnMapReadyCal
     @Override
     public final void onMapReady(final GoogleMap map) {
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-        for(final File f : new File(Environment.DIRECTORY_DOWNLOADS).listFiles())
-            System.out.println(f.getAbsolutePath());
-
-        final File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "opdracht.kml");
-        load(f);
     }
 
     /**
@@ -68,9 +55,9 @@ public final class MapActivity extends FragmentActivity implements OnMapReadyCal
             System.out.println("Line: " + s);
         }
 
-        final Polygon polygon = map.addPolygon(new PolygonOptions().addAll(positions));
-        polygon.setStrokeColor(Color.GREEN);
-        polygon.setStrokeWidth(0.6F);
+        // final Polygon polygon = map.addPolygon(new PolygonOptions().addAll(positions));
+        // polygon.setStrokeColor(Color.GREEN);
+        //  polygon.setStrokeWidth(0.6F);
         return true;
     }
 }
