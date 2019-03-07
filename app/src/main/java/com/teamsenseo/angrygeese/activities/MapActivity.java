05d4e15a40be0ebb2f3d893d4c6203d466350360
Robert <robertkremer.frl@gmail.com>
2019-03-07 09:23:36 +0100

    New thing
    oof

--- app/src/main/java/com/teamsenseo/angrygeese/activities/MapActivity.java
+++ app/src/main/java/com/teamsenseo/angrygeese/activities/MapActivity.java
@@ -1,5 +1,6 @@
 package com.teamsenseo.angrygeese.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +8,9 @@ import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.teamsenseo.angrygeese.AngryGeese;
 import com.teamsenseo.angrygeese.R;
 
@@ -33,5 +37,18 @@ public final class MapActivity extends FragmentActivity implements OnMapReadyCal
     public final void onMapReady(final GoogleMap map) {
         AngryGeese.instance = new AngryGeese(getApplication(), map);
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        final PolygonOptions options = new PolygonOptions();
+        options.add(new LatLng(52.532327431071, 4.9698428582864),
+                new LatLng(52.5363890459246, 4.97152173643456),
+                new LatLng(52.5359060240486, 4.96928150387894),
+                new LatLng(52.5334104595542, 4.96744278341884),
+                new LatLng(52.5314914623206, 4.96633040367443),
+                new LatLng(52.5319261071537, 4.96651718547247),
+                new LatLng(52.5321208961933, 4.96596108874737),
+                new LatLng(52.532327431071, 4.96596108874737));
+
+        options.fillColor(Color.GREEN);
+        final Polygon polygon = map.addPolygon(options);
     }
 }
